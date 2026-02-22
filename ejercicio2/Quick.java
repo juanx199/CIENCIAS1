536c6648e531e9f586ff9e1a6bc9786be2a59c34
vzavna <avnessa222@gmail.com>
2026-02-22 17:04:14 -0500

    Update Quick.java

--- ejercicio2/Quick.java
+++ ejercicio2/Quick.java
@@ -9,5 +9,45 @@ package sorts;
  * @author JUANCA
  */
 public class Quick {
-    
+    public class Quick {
+    public long pasos = 0;
+    public long ciclos = 0;
+
+    public void ordenar(int[] arr, int low, int high) {
+        if (low < high) {
+            pasos++; // Condicional
+            int pi = partition(arr, low, high);
+            ordenar(arr, low, pi - 1);
+            ordenar(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        pasos++; // Asignación del pivote
+        int i = (low - 1);
+        
+        for (int j = low; j < high; j++) {
+            ciclos++; // Iteración del bucle
+            if (arr[j] < pivot) {
+                pasos++; // Condicional
+                i++;
+                // Swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                pasos += 3; // 3 pasos por el intercambio
+            }
+        }
+
+        // Swap final con el pivote
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        pasos += 3;
+        
+        return i + 1;
+    }
 }
+}
+
