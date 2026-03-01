34ddd0c542b14e1dec341cf629188feebc988e19
vzavna <avnessa222@gmail.com>
2026-03-01 18:52:21 -0500

    Update Main.java

--- SudokuSuma/src/sudokusuma/Main.java
+++ SudokuSuma/src/sudokusuma/Main.java
@@ -9,17 +9,32 @@ package sudokusuma;
  * @author JUANCA
  */
 public class Main {
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
+        // --- PARTE 1: SUDOKU (Fuerza Bruta) ---
         Sudoku juego = new Sudoku();
-
-        System.out.println("Generando el tablerito aleatorio .....");
-
-        if (juego.solver(){
+        System.out.println("=== EJECUTANDO SUDOKU 4x4 (Fuerza Bruta) ===");
+        
+        if (juego.solver()) {
             juego.imprimir();
         }
-        System.out.println();
         juego.imprimirResultados();
+
+        System.out.println("\n-------------------------------------------\n");
+
+        // --- PARTE 2: SUMA SUBSECUENTE (Divide y Vencerás) ---
+        System.out.println("=== EJECUTANDO SUMA SUBSECUENTE (Divide y Vencerás) ===");
+        int[] arregloEjemplo = {-2, -5, 6, -2, -3, 1, 5, -6};
+        
+        SumaMaxima sm = new SumaMaxima();
+        int resultado = sm.calcular(arregloEjemplo, 0, arregloEjemplo.length - 1);
+
+        System.out.print("Arreglo analizado: [");
+        for(int i=0; i < arregloEjemplo.length; i++) 
+            System.out.print(arregloEjemplo[i] + (i == arregloEjemplo.length-1 ? "" : ", "));
+        System.out.println("]");
+        
+        System.out.println("La suma subsecuente más grande es: " + resultado);
     }
 }
+
 
