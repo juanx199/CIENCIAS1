41b6941510b1e709132918278b72bc389441c6ba
juanx199 <132611235+juanx199@users.noreply.github.com>
2026-03-08 17:54:49 -0500

    Add files via upload

--- Moneda_Sudoku9x9/src/moneda_sudoku9x9/SudokuSolver.java
+++ Moneda_Sudoku9x9/src/moneda_sudoku9x9/SudokuSolver.java
@@ -9,37 +9,60 @@ package moneda_sudoku9x9;
  * @author JUANCA
  */
 public class SudokuSolver {
-    public static boolean resolver(int[][] tablero) {
+    public static boolean resolver(int[][] tablero, long[] contador) {
+        contador[0]++ ; //cada que se entre se suma un paso
+        
         for (int fila = 0; fila < 9; fila++) {
             for (int col = 0; col < 9; col++) {
-                if (tablero[fila][col] == 0) { // Celda vacía
+                if (tablero[fila][col] == 0) { //Buscar una casilla vacía
                     for (int num = 1; num <= 9; num++) {
-                        if (esSeguro(tablero, fila, col, num)) {
+                        if (esValido(tablero, fila, col, num)) {
                             tablero[fila][col] = num;
-                            if (resolver(tablero)) return true;
-                            tablero[fila][col] = 0; // Se marca como absurdo y se limpia
+                            // Seguir con el resto 
+                            if (resolver(tablero, contador)) return true;
+                            
+                            tablero[fila][col] = 0; // Se marca como malo y se hace backtrackin
                         }
                     }
-                    return false; // No hubo número válido, retrocede
+                    return false; // No hubo número válido entonces retrocedemos
                 }
             }
         }
         return true; // Tablero lleno
     }
 
-    private static boolean esSeguro(int[][] b, int f, int c, int n) {
+    private static boolean esValido(int[][] t, int fila, int col, int num) {
         for (int i = 0; i < 9; i++) {
-            // Verificar fila, columna y sub-recuadro 3x3
-            if (b[f][i] == n || b[i][c] == n || 
-                b[f - f % 3 + i / 3][c - c % 3 + i % 3] == n) return false;
+            if(t[fila][i] == num || t[i][col]==num) return false;
+           
+        }
+        int iniF = (fila /3)*3;
+        int iniC = (col /3)*3;
+        for(int i = 0 ; i<3; i++){
+            for (int j = 0; j<3 ; j++){
+                if (t[iniF +i][iniC + j]==num) return false;
+            }
         }
         return true;
     }
     
     public static void imprimirTablero(int[][] tablero) {
+        System.out.println("\n ### Solución ###");
+        System.out.println("-----------------------");
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) System.out.print(tablero[i][j] + " ");
-            System.out.println();
-        }
+            if (i % 3 == 0 && i != 0){
+                System.out.println("------+-------+------");
+            }
+            for (int j = 0; j < 9; j++){
+                
+                if(j %3 == 0 && j != 0){
+                    System.out.print("| ");
+                }
+                
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println("|");
+        }    
+        System.out.println("------------------------");
     }
 }
