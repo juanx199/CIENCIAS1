0d38e2b225ac3832e5011a3c54003c01105d7099
juanx199 <jccarvajalcamargo@gmail.com>
2026-03-21 23:18:52 -0500

    Configurar los pez

--- PecesyMatriz/src/peces/SolucionPez.java
+++ PecesyMatriz/src/peces/SolucionPez.java
@@ -6,21 +6,20 @@ package peces;
 
 /**
  *
- * @author Estudiantes
+ * @author Juanca
  */
-
-
 
 public class SolucionPez {
 
     public static int calcularMaximoSabor(int n, int m, int[][] matriz) {
         // dp[j] almacenará el máximo sabor para j dulces tomados
-        // Usamos un array unidimensional para ahorrar espacio (optimizando la fila anterior)
+        // Usamos un array para ahorrar espacio 
         int[] dp = new int[m + 1];
-
+        System.out.println("=== MEMORIA DP ===");
+        System.out.println("Buscando el sabor máximo para " + m + "dulces");
         // Procesamos cada dispensador uno por uno
         for (int i = 0; i < n; i++) {
-            // Calculamos sumas acumuladas para el dispensador actual (0 a 12 dulces)
+            // Calculamos sumas acumuladas para el dispensador actual 
             int[] sumasPrefijo = new int[13];
             int sumaActual = 0;
             for (int k = 1; k <= 12; k++) {
@@ -35,8 +34,25 @@ public class SolucionPez {
                         dp[j] = Math.max(dp[j], dp[j - k] + sumasPrefijo[k]);
                     }
                 }
+                
+            }
+            //MOstrar la memoria
+            if (i < 5 || i == n-1){
+                imprimirFilaCorta(i +1 , dp);
+            } else if (i == 5){
+                System.out.println(" procesando dispensadores intermedios ... ");
             }
         }
+             
         return dp[m];
+    }
+    private static void imprimirFilaCorta(int id, int[] dp){
+        System.out.print("Dispensador #"+ id + " Memoria:[0, ");
+        
+        int elementos = Math.min(dp.length, 5);
+        for (int i = 0; i < elementos; i++){
+            System.out.print(dp[i] + ", ");
+        }
+        System.out.println(" ... " + dp[dp.length - 1] + "]");
     }
 }
