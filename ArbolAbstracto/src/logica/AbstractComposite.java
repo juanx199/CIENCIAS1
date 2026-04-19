a392822b69971d8c3417064daa70cb6d0a724dd3
vzavna <avnessa222@gmail.com>
2026-04-18 20:21:21 -0500

    Update AbstractComposite.java

--- ArbolAbstracto/src/logica/AbstractComposite.java
+++ ArbolAbstracto/src/logica/AbstractComposite.java
@@ -1,24 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logica;
+
+import java.util.List;
 
 public abstract class AbstractComposite {
     protected String nombre;
-    public String getNombre(){
-        return nombre;
-    }
-    public AbstractComposite(String nombre){
+    
+    public AbstractComposite(String nombre) {
         this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
     }
     
     public abstract void mostrarDetalle(int profundidad);
     
-    public void agregar(AbstractComposite componente){
-        throw new UnsupportedOperationException("No se puede hacer en este nodo");
+    public abstract boolean esHoja();
+    
+    public void agregar(AbstractComposite componente) {
+        throw new UnsupportedOperationException("Este nodo no puede tener hijos");
     }
-    public void eliminar(AbstractComposite componente){
-        throw new UnsupportedOperationException("No se puede hacer en este nodo");
+    
+    public void eliminar(AbstractComposite componente) {
+        throw new UnsupportedOperationException("Este nodo no puede tener hijos");
+    }
+    
+    public List<AbstractComposite> getHijos() {
+        throw new UnsupportedOperationException("Este nodo no tiene hijos");
     }
 }
