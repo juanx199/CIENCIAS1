ffeeb87fc5794cec8cfb5486bd440ff201c9bbcd
vzavna <avnessa222@gmail.com>
2026-04-18 20:21:09 -0500

    Update Raiz.java

--- ArbolAbstracto/src/componentes/Raiz.java
+++ ArbolAbstracto/src/componentes/Raiz.java
@@ -1,40 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package componentes;
 
 import logica.AbstractComposite;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author JUANCA
- */
 public class Raiz extends AbstractComposite {
-    private List<AbstractComposite> hijos = new ArrayList<>();
-
+    private List<AbstractComposite> hijos;
+    
     public Raiz(String nombre) {
         super(nombre);
+        this.hijos = new ArrayList<AbstractComposite>();
     }
-
+    
     @Override
     public void agregar(AbstractComposite componente) {
         hijos.add(componente);
-
+        System.out.println("  -> Se agrego '" + componente.getNombre() + "' a '" + this.nombre + "'");
     }
-
+    
     @Override
     public void eliminar(AbstractComposite componente) {
-        hijos.remove(componente);
+        if(hijos.remove(componente)) {
+            System.out.println("  -> Se elimino '" + componente.getNombre() + "' de '" + this.nombre + "'");
+        } else {
+            System.out.println("  -> No se encontro el componente");
+        }
     }
-
+    
+    @Override
+    public List<AbstractComposite> getHijos() {
+        return hijos;
+    }
+    
     @Override
     public void mostrarDetalle(int profundidad) {
-        System.out.println(" ".repeat(profundidad) + "[Raiz/Rama]" + nombre);
-        for (AbstractComposite hijo : hijos) {
+        String espacios = "";
+        for(int i = 0; i < profundidad; i++) {
+            espacios = espacios + "  ";
+        }
+        System.out.println(espacios + "+ " + nombre + " (Raiz/Rama)");
+        
+        for(AbstractComposite hijo : hijos) {
             hijo.mostrarDetalle(profundidad + 1);
         }
+    }
+    
+    @Override
+    public boolean esHoja() {
+        return false;
     }
 }
